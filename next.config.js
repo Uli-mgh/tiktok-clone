d33a1fecafb76182734a1d4baee9fde80dde9da5
Uli-mgh <ulicmgh@gmail.com>
2022-07-12 05:43:06 -0300

    part one

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["pps.whatsapp.net", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
